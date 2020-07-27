3ca64e639d20254c5cf94d4f52dea5560c2e7629
Weapon Lin <weaponlin@126.com>
2020-07-27 11:06:41 +0800

    properties: define

--- prpc-all/src/main/java/com/github/weaponlin/prpc/config/PConfig.java
+++ prpc-all/src/main/java/com/github/weaponlin/prpc/config/PConfig.java
@@ -7,22 +7,47 @@ import java.util.List;
 @Data
 public class PConfig {
 
+    /**
+     * global zookeeper address and path, eg: zookeeper://127.0.0.1:2181/prpc
+     */
     private String zookeeper;
 
+    /**
+     * unit: milliseconds
+     */
     private int connectionTimeout;
 
+    /**
+     * fault tolerant of cluster
+     */
     private String cluster;
 
+    /**
+     * load balance
+     */
     private String loadBalance;
 
+    /**
+     * data encode&decode
+     * optional:
+     */
     private String codec;
 
-    private List<PService> services;
+    /**
+     * service groups configuration
+     */
+    private List<PGroup> groups;
 
     @Data
-    public static class PService {
+    public static class PGroup {
+        /**
+         * service group
+         */
         private String group;
 
+        /**
+         * zookeeper address of a specified group, if null or blank then will use the global zookeeper
+         */
         private String zookeeper;
 
         private int connectionTimeout;
